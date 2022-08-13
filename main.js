4d440a84320c80c167492ab1516a7c71fafbadb3
adviktyagi1234 <92938197+adviktyagi1234@users.noreply.github.com>
2022-08-13 20:21:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ LeftWristX = 0;
 LeftWristY = 0;
 RightWristX = 0;
 RightWristY = 0;
+ScoreLeftWrist = 0;
+PlayOrNot = "";
 
 function preload() {
     song1 = loadSound("Song2.mp3");
@@ -30,16 +32,36 @@ function gotPoses(results)
 {
     if(results[0] > 0)
     {
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+        RightWristX = results[0].pose.rightWrist.x;
+        RightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + RightWristX + " rightWristY = " + RightWristY);
 
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+        LeftWristX = results[0].pose.leftWrist.x;
+        LeftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + LeftWristX + " leftWristY = " + LeftWristY);
+
+        ScoreLeftWrist = resluts[0].pose.keypoints[9].score;
     }
 }
 
 function draw() {
     image(video, 0, 0, 600, 500)
+
+     
+    PlayOrNot = song1.isPlaying();
+
+    fill("Red");
+    stroke("Black");
+
+    if(LeftWristX > 0.2)
+    {  
+    circle(LeftWristX, LeftWristY, 10000);
+    song2.stop();
+
+    if(PlayOrNot = false)
+    {
+        song1.play();
+        document.getElementById("snnm").innerHTML = "Song Name = " + song1;
+    }
+    }
 }
